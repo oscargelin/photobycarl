577b2b03eb899abdb15c905df8a104aa8fb27568
oscargelin <oscargelin@gmail.com>
2023-08-31 00:02:36 +0200

    update contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,35 @@
 import React from "react";
 
 const page = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <h2>Contact me</h2>
+      <p>
+        Based in Stockholm. All prices agreed upon individually based on your
+        preferred total package. My main focus as a photographer is to reflect
+        the right feeling and atmosphere from your event, may it be a banquet, a
+        couples portrait at home. an event at the racetrack or simply a
+        carshoot.
+      </p>
+      <p>
+        I work a lot with angles, colors and the natural lighting to reflect
+        that special feeling combined with using foreground and background to
+        provide a sense of depth to the picture I always provide edited high
+        resolution photos per default, but I can also provide original unedited
+        versions if preferred. I can also provide physically framed photos set
+        done and ready to hang up on your living room wall. Standard formats are
+        50*70 cm or 60*80 cm. But can also offer custom measurements and custom
+        frames.
+      </p>{" "}
+      <p>
+        This is a slightly more "premium" service and thus slightly higher
+        priced. But something I highly recommend in order to give that personal
+        touch to your home whilst at the same time preserving a special memory
+        to show for your friends and family. Don't hesitate to contact me for
+        further dialogue.
+      </p>
+    </div>
+  );
 };
 
 export default page;
